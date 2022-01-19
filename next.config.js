8659723d497c3b44f12a29726e4f2ed32712b693
Igor Bondarenko <ibondarenko@reebee.com>
2022-01-19 18:38:46 -0500

    :bug: asd

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
     trailingSlash: true,
+    async rewrites() {
+        return [
+            {
+                source: "/blog/:slug*",
+                destination: "https://test2-ten-beta.vercel.app/blog/:slug*",
+            },
+        ];
+    },
 };
